1349a8b481a778d9a8bda186dc21113803a6c2f8
yuechu <304723403@qq.com>
2023-04-29 21:44:24 +0800

    模块建立，测试mp接口完成

---
+++ sg-blog/src/main/java/com/sangeng/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.sanggeng;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.sangeng.mapper")
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
