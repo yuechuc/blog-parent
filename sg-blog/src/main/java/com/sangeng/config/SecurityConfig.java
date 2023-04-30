f89e4db42f264cafdda7abd2f0d1d64b9a69dfe7
yuechu <304723403@qq.com>
2023-04-30 21:32:14 +0800

    整合Security 1.2登录校验过滤器代码实现

--- sg-blog/src/main/java/com/sangeng/config/SecurityConfig.java
+++ sg-blog/src/main/java/com/sangeng/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sangeng.config;
 
+import com.sangeng.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -21,6 +22,8 @@ public class SecurityConfig  {
     //private CustomUserDetailsService userDetailsService;
 
 
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
     //创建AuthenticationManager注入容器
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -43,12 +46,16 @@ public class SecurityConfig  {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                //测试认证token，测试完可以删除
+                .antMatchers("/link/getAllLink").authenticated()
                 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
 
         http.logout().disable();
         //允许跨域
         http.cors();
+        //添加过滤器在UsernamePasswordAuthenticationFilter之前
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
