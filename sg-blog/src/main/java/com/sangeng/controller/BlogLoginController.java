4ca115d3f9923d12f2ea0102edbb3d267f2dd4e8
yuechu <304723403@qq.com>
2023-04-30 22:41:10 +0800

    整合Security 2.添加认证失败处理器，全局异常处理器 3.退出登录的实现

--- sg-blog/src/main/java/com/sangeng/controller/BlogLoginController.java
+++ sg-blog/src/main/java/com/sangeng/controller/BlogLoginController.java
@@ -1,9 +1,12 @@
 package com.sangeng.controller;
 
+import com.sangeng.enums.AppHttpCodeEnum;
+import com.sangeng.exception.SystemException;
 import com.sangeng.response.ResponseResult;
 import com.sangeng.service.BlogLoginService;
 import com.sangeng.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sangeng.domian.User;
 
 @RestController
-@RequestMapping("/login")
+
 public class BlogLoginController {
     @Autowired
     private BlogLoginService blogLoginService;
-    @PostMapping
+    @PostMapping("/login")
     public ResponseResult login(@RequestBody User user){
+        if (!StringUtils.hasText(user.getUserName())){
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
      return blogLoginService.login(user);
     }
+    @PostMapping("/logout")
+    public ResponseResult logout(){
+
+        return blogLoginService.logout();
+    }
+
+
 }
