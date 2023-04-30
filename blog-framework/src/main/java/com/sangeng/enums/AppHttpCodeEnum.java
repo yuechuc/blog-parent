b5d10c723eadba1484cd9ad5f8c9c5e8d149abc9
yuechu <304723403@qq.com>
2023-05-01 04:20:14 +0800

    头像上传接口 上传到七牛OSS

--- blog-framework/src/main/java/com/sangeng/enums/AppHttpCodeEnum.java
+++ blog-framework/src/main/java/com/sangeng/enums/AppHttpCodeEnum.java
@@ -12,6 +12,9 @@ public enum AppHttpCodeEnum {
     PHONENUMBER_EXIST(502,"手机号已存在"), EMAIL_EXIST(503, "邮箱已存在"),
     REQUIRE_USERNAME(504, "必需填写用户名"),
     LOGIN_ERROR(505,"用户名或密码错误"),
+    FILE_TYPE_ERROR(507,"文件类型错误"),
+
+
     CONTEXT_NOT_NULL(506,"评论不能为空");
 
     int code;
--- sg-blog/src/main/java/com/sangeng/config/SecurityConfig.java
+++ sg-blog/src/main/java/com/sangeng/config/SecurityConfig.java
@@ -52,8 +52,10 @@ public class SecurityConfig  {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                .antMatchers("/upload").anonymous()
                 //测试认证token，测试完可以删除
                 .antMatchers("/user/userInfo").authenticated()
+                .antMatchers("/upload").anonymous()
                 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
         //关闭默认的注销功能
