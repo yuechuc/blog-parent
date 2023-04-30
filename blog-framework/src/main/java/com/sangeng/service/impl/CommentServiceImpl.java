2f529a2ac44eca439980916782809e4b273bde44
yuechu <304723403@qq.com>
2023-05-01 00:18:33 +0800

    查询评论列表接口  1.3查询子评论

--- blog-framework/src/main/java/com/sangeng/service/impl/CommentServiceImpl.java
+++ blog-framework/src/main/java/com/sangeng/service/impl/CommentServiceImpl.java
@@ -43,15 +43,34 @@ public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment>
         Page<Comment> page = new Page<>(pageNum, pageSize);
         Page<Comment> commentPage = this.page(page, queryWrapper);
         List<Comment> comments = commentPage.getRecords();
-
-
         List<CommentVo> commentVos =toCommentVoList(comments);
+
+        //查询子评论
+        for (CommentVo commentVo : commentVos) {
+            List<CommentVo> chidren = getChidren( commentVo.getRootId());
+            commentVo.setChildren(chidren);
+        }
         PageVo pageVo = new PageVo(commentVos, commentPage.getTotal());
 
         return ResponseResult.okResult(pageVo);
     }
 
 
+   //根据根评论的id查询所对应的子评论的集合
+   //rootId 根评论的id
+    private List<CommentVo> getChidren(Long rootId) {
+        LambdaQueryWrapper<Comment> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper
+                .eq(Comment::getRootId,rootId)
+                .orderByAsc(Comment::getCreateTime);
+        List<Comment> list = this.list(queryWrapper);
+        List<CommentVo> commentVos = toCommentVoList(list);
+        return commentVos;
+    }
+
+
+    //写入被回复评论的nickName
+    //和评论创建人的nickName
     private List<CommentVo> toCommentVoList(List<Comment> commentList){
         List<CommentVo> commentVos = BeanCopyUtils.copyBeanList(commentList, CommentVo.class);
         for (CommentVo commentVo : commentVos) {
